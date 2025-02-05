dd0f77497ee94174d08b9fff3ca48723c39af1df
D1JS <arturas.mickevicius@stud.techin.lt>
2025-02-05 12:53:45 +0200

    Change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
 
 const Navbar = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <nav className="flex items-center justify-between bg-gray-800 text-white py-4 px-6">
       <div className="flex items-center">
@@ -9,17 +13,23 @@ const Navbar = () => {
       <div className="flex items-center">
         <ul className="flex space-x-4">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/">Home</NavLink>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
+
+          {!user ? (
+            <>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup">Signup</NavLink>
+              </li>
+            </>
+          ) : (
+            <li>
+              <NavLink to="/logout">Logout</NavLink>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
